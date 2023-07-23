366fc665b93a24237e1006771ad8318b97315b75
thodsp <thodbusiness@gmail.com>
2023-07-23 18:14:57 +0300

    Change Text

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,7 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    setSize (600, 400);
+    setSize (900, 300);
 }
 
 MainComponent::~MainComponent()
@@ -16,9 +16,9 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.setFont(juce::Font(128.0f));
+    g.setColour (juce::Colours::greenyellow);
+    g.drawText ("It Begins", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
